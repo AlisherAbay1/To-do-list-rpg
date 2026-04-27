c0ab029dc1ae3add265a6ce129bd1cd75186008e
Alisher <alisherggcom@gmail.com>
2026-04-27 17:06:21 +0500

    fix: alembic can autogenerate now

--- src/app/infrastructure/database/models/__init__.py
+++ src/app/infrastructure/database/models/__init__.py
@@ -1,19 +1,9 @@
 from .base import Base
 from .relations import Tasks_to_items, Tasks_to_skills, Items_to_skills, \
                         Tasks_history_to_items, Tasks_history_to_skills
-from .users import User, Rank
-from .tasks import Task, TaskCategory, TaskHistory
-from .skills import Skill
-from .shop import Shop
-from .items import Item, ItemUsageHistory
-from .inventory import Inventory
 
 
 __all__ = (
     "Base", "Tasks_to_items", "Tasks_to_skills",
-    "Items_to_skills", "Tasks_history_to_items", "Tasks_history_to_skills",
-    "User", "Rank", "Task",
-    "TaskCategory", "TaskHistory", "Skill",
-    "Shop", "Item", "ItemUsageHistory",
-    "Inventory",
+    "Items_to_skills", "Tasks_history_to_items", "Tasks_history_to_skills"
 )
