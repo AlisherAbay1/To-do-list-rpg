d0cbcde0ed659556c34b0916645610e2c601105f
Alisher <alisherggcom@gmail.com>
2026-05-06 13:40:48 +0500

    feat: add inventory field and CRUD template
    - Add new field to inventory table
    - Add inventory CRUD template (WIP)

--- src/app/domain/inventory.py
+++ src/app/domain/inventory.py
@@ -1,4 +1,5 @@
 from uuid import UUID
+from uuid6 import uuid7
 
 from sqlalchemy import BigInteger, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
@@ -9,6 +10,7 @@ from src.app.infrastructure.database.models.base import Base
 class Inventory(Base, kw_only=True):
     __tablename__ = "inventory"
 
-    user_id: Mapped[UUID] = mapped_column(ForeignKey("user.id", ondelete="CASCADE"), primary_key=True)
-    item_id: Mapped[UUID] = mapped_column(ForeignKey("item.id", ondelete="CASCADE"), primary_key=True)
+    id: Mapped[UUID] = mapped_column(default_factory=uuid7, primary_key=True)
+    user_id: Mapped[UUID] = mapped_column(ForeignKey("user.id", ondelete="CASCADE"))
+    item_id: Mapped[UUID] = mapped_column(ForeignKey("item.id", ondelete="CASCADE"))
     quantity: Mapped[int] = mapped_column(BigInteger)
--- src/app/presentation/api/routers/inventory.py
+++ src/app/presentation/api/routers/inventory.py
@@ -1,3 +1,55 @@
-from fastapi import APIRouter, Depends, HTTPException, Request
+from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Cookie
+from dishka.integrations.fastapi import DishkaRoute, FromDishka
+from src.app.application.interactors import (
+    
+    )
+from src.app.presentation.schemas import 
+from src.app.presentation.mappers import InventorySchemaMapper
+from uuid import UUID
 
-router = APIRouter(prefix="/inventory")
+router = APIRouter(prefix="/inventory", route_class=DishkaRoute)
+
+@router.get("/me", response_model=...)
+async def get_current_user_inventory_item(interactor: FromDishka[...], 
+                                         session_token = Cookie(None), 
+                                         limit: int = 20, 
+                                         offset: int = 0):
+    if session_token is None:
+        raise HTTPException(401, "Not authenticated")
+    return await interactor(session_token, limit, offset)
+
+@router.get("/me/{inventory_item_id}", response_model=...)
+async def get_current_user_inventory_item_by_id(interactor: FromDishka[...], 
+                                              inventory_item_id: UUID,
+                                              session_token = Cookie(None)):
+    if session_token is None:
+        raise HTTPException(401, "Not authenticated")
+    return await interactor(session_token, inventory_item_id)
+
+@router.post("/me", response_model=...)
+async def create_current_user_inventory_itemg(interactor: FromDishka[...], 
+                                           schema: ...,
+                                           session_token = Cookie(None)):
+    if session_token is None:
+        raise HTTPException(401, "Not authenticated")
+    dto = InventorySchemaMapper.to_create_dto(schema)
+    return await interactor(session_token, dto)
+
+@router.patch("/me/{inventory_item_id}", response_model=...)
+async def update_current_user_inventory_item(inventory_item_id: UUID, 
+                                           interactor: FromDishka[...], 
+                                           schema: ...,
+                                           session_token = Cookie(None)):
+    if session_token is None:
+        raise HTTPException(401, "Not authenticated")
+    dto = InventorySchemaMapper.to_update_dto(schema)
+    return await interactor(inventory_item_id, session_token, dto)
+
+@router.delete("/me/{inventory_item_id}", status_code=204)
+async def delete_current_user_inventory_item(inventory_item_id: UUID, 
+                                           interactor: FromDishka[...], 
+                                           session_token = Cookie(None)):
+    if session_token is None:
+        raise HTTPException(401, "Not authenticated")
+    return await interactor(inventory_item_id, session_token)
